b316f746c91ff040ab5eeafb241de4d4b1935867
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-04-07 17:50:05 +0700

    add associaton between user and relationship

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,7 @@
 
 class Relationship < ApplicationRecord
   enum :status, { pending: 0, accepted: 1 }
+
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :follower_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy,
+                                    inverse_of: :follower
+  has_many :following_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy,
+                                      inverse_of: :following
+  has_many :followers, through: :follower_relationships, class_name: "User"
+  has_many :following, through: :following_relationships, source: :followed, class_name: "User"
 end
