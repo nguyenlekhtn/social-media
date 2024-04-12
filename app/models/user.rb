d01536a46b4d7c936931b5ad55fe4f2946794238
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-04-12 21:41:10 +0700

    fix User associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
+require_relative 'relationship'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :follower_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy,
+  has_many :follower_relationships, class_name: 'Relationship', dependent: :destroy,
                                     inverse_of: :follower
-  has_many :following_relationships, foreign_key: 'user_id', class_name: 'Relationship', dependent: :destroy,
-                                      inverse_of: :following
-  has_many :followers, through: :follower_relationships, class_name: "User"
-  has_many :following, through: :following_relationships, source: :followed, class_name: "User"
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy,
+                                     inverse_of: :user
+  has_many :followers, through: :follower_relationships, class_name: 'User'
+  has_many :following, through: :following_relationships, source: :user, class_name: 'User'
+
+  def following?(user)
+    Relationship.where(user:, follower: self, status: 'A').any?
+  end
 end
