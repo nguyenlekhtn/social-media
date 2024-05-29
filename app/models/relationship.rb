9c6af8cafa7f1368b07f40bcb6a6553f3d34aee1
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-05-29 21:02:16 +0700

    rename follower to requester

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,6 @@
 class Relationship < ApplicationRecord
+  belongs_to :requester, class_name: 'User'
+  belongs_to :requested_user, class_name: 'USer'
+
+  validates :requester, uniqueness: { scope: :requested_user }
 end
---
+++ db/migrate/20240529134426_create_relationships.rb
@@ -0,0 +1,11 @@
+class CreateRelationships < ActiveRecord::Migration[7.1]
+  def change
+    drop_table :relationships # rubocop:disable Rails/ReversibleMigration
+    create_table :relationships do |t|
+      t.references :requester, foreign_key: { to_table: :users }
+      t.references :requested_user, foreign_key: { to_table: :users }
+      t.integer :status, default: 0
+      t.timestamps
+    end
+  end
+end
