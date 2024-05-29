08f542035a8676a0c067285c51b75577dbc9c4f9
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-05-29 20:28:27 +0700

    replace table requests with Relationships

--- db/migrate/20240528105230_create_relationships.rb
+++ db/migrate/20240528105230_create_relationships.rb
@@ -1,8 +1,9 @@
 class CreateRelationships < ActiveRecord::Migration[7.1]
   def change
+    drop_table :requests # rubocop:disable Rails/ReversibleMigration
     create_table :relationships do |t|
       t.references :follower, foreign_key: { to_table: :users }
-      t.references :followed, foreign_key: { to_table: :users }
+      t.references :followed_user, foreign_key: { to_table: :users }
       t.integer :status, default: 0
       t.timestamps
     end
