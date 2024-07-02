a4f7a9dc97242f6a84cecdb4bd17c8f57c5abed6
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-07-02 21:49:55 +0700

    add primary key for FollowRelationships table

--- db/migrate/20240702141834_create_follow_relationships.rb
+++ db/migrate/20240702141834_create_follow_relationships.rb
@@ -1,6 +1,6 @@
 class CreateFollowRelationships < ActiveRecord::Migration[7.1]
   def change
-    create_table :follow_relationships do |t|
+    create_table :follow_relationships, primary_key: %i[follower_id followed_user_id] do |t|
       t.references :follower, foreign_key: { to_table: :users }
       t.references :followed_user, foreign_key: { to_table: :users }
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_02_141834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "follow_relationships", force: :cascade do |t|
-    t.bigint "follower_id"
-    t.bigint "followed_user_id"
+  create_table "follow_relationships", primary_key: ["follower_id", "followed_user_id"], force: :cascade do |t|
+    t.bigint "follower_id", null: false
+    t.bigint "followed_user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["followed_user_id"], name: "index_follow_relationships_on_followed_user_id"
