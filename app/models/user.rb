c52379d696b330f02147941ccb6d30eb9160320b
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-07-02 22:15:53 +0700

    add associations to FollowRelationhip and User

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,13 @@ class User < ApplicationRecord
 
   has_many :received_requests, foreign_key: 'requested_user_id', class_name: 'Request', dependent: :destroy,
                                inverse_of: :requested_user
+
+  has_many :relationship_as_follower, foreign_key: 'follower_id', class_name: 'FollowRelationship',
+                                      dependent: :destroy, inverse_of: :follower
+
+  has_many :relationship_as_followed, foreign_key: 'follower_id', class_name: 'FollowRelationship',
+                                      dependent: :destroy, inverse_of: :followed_user
+
+  has_many :followers, through: :relationship_as_followed, source: :follower
+  has_many :followed_users, through: :relationship_as_follower, source: :followed_user
 end
