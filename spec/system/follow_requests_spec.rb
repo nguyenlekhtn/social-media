8bff241e9048769cf39588a6652c465a833fc949
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-07-03 22:42:44 +0700

    create 'when current user visit an unfamilar user' system test

--- spec/system/follow_requests_spec.rb
+++ spec/system/follow_requests_spec.rb
@@ -5,9 +5,15 @@ RSpec.describe 'FollowRequests', type: :system do
     driven_by(:rack_test)
   end
 
-  context 'when current user visit an unfollowing user with no pending request' do
+  context 'when current user visit an unfamilar user' do
+    let(:current_user) { create(:user) }
+    let(:unfamilar_user) { create(:user) }
+
     it 'has a follow button in the profile' do
-      sign_in
+      sign_in current_user
+
+      visit users_path(unfamilar_user)
+      expect(page).to have_button('Follow')
     end
   end
 end
