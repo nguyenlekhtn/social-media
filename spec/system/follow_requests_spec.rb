3679d883ace2c8156d099e6d7fe446924869125a
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-07-03 22:12:04 +0700

    create User factoryBot

--- spec/system/follow_requests_spec.rb
+++ spec/system/follow_requests_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe 'FollowRequests', type: :system do
     driven_by(:rack_test)
   end
 
-  context "when current user click Follow button in a unfollowed user's profile" do
-    it 'creates a new pending follow request' do
-      click_on 'Follow'
+  context 'when current user visit an unfollowing user with no pending request' do
+    it 'has a follow button in the profile' do
+      sign_in
     end
   end
 end
