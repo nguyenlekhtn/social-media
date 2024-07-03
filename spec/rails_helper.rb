3bbcf6b5754cca009e597091db20e63c6b14492d
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-07-03 22:51:04 +0700

    set up config to us devise test helpers on rspec tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require_relative 'support/factory_bot'
+require_relative 'support/devise'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
