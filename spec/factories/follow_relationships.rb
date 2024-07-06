f72df9c16ddb62f2f76914a66b50812c20963dbc
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-07-06 09:41:25 +0700

    create and make test that verify Follow show up correctly, pass

--- spec/factories/follow_relationships.rb
+++ spec/factories/follow_relationships.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :follow_relationship do
-    
+    requester factory: %i[user]
+    requested_user factory: %i[user]
   end
 end
--- spec/system/follow_requests_spec.rb
+++ spec/system/follow_requests_spec.rb
@@ -5,15 +5,31 @@ RSpec.describe 'FollowRequests', type: :system do
     driven_by(:rack_test)
   end
 
-  context 'when current user visit an unfamilar user' do
+  context 'when current user visits an unfamilar user' do
     let(:current_user) { create(:user) }
     let(:unfamilar_user) { create(:user) }
 
     it 'has a follow button in the profile' do
       sign_in current_user
 
-      visit users_path(unfamilar_user)
+      visit user_path(unfamilar_user)
       expect(page).to have_button('Follow')
+    end
+  end
+
+  context 'when current user visits an user who received requests from him but not accepted yet' do
+    let(:current_user) { create(:user) }
+    let(:pending_user) { create(:user) }
+
+    before do
+      create(:request, requester: current_user, requested_user: pending_user)
+    end
+
+    it 'has a pending button in the profile that is disabled' do
+      sign_in current_user
+
+      visit user_path(pending_user)
+      expect(page).to have_button('Pending', disabled: true)
     end
   end
 end
